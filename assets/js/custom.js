c84bfe26ab2fefa24ebb57a2e3799ccf71c92016
Boiled-front <oleksandrneschasnij@gmail.com>
2023-02-22 19:23:18 +0200

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+const sidebarButton = document.querySelector('.sidebar-button');
+const sidebarMenu = document.querySelector('.sidebar');
+
+sidebarButton.addEventListener('click', function(){
+    sidebarMenu.classList.toggle('active');
+    sidebarButton.classList.toggle('active');
+})
+
+
